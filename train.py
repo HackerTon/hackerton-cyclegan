3735627b3dfc49770380be92dfddc7fa9961d647
hackerton <wizardlordlam@gmail.com>
2020-07-12 20:52:52 +0800

    added history training

--- train.py
+++ train.py
@@ -1,20 +1,55 @@
-import tensorflow as tf
-import model
-import numpy as np
-import datetime
-import time
 import argparse
+import datetime
 import os
+import random
+import time
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import python
 from tensorflow_examples.models.pix2pix import pix2pix
 
+import model
 
 NUM_ITERATION = 100
-LAMBDA = tf.constant(5, tf.float32)
-TAKEN_NUM = 5000
+LAMBDA = tf.constant(10, tf.float32)
+TAKEN_NUM = 1000
 BETA_1 = 0.5
 AUTOTUNE = tf.data.experimental.AUTOTUNE
+LEARNING_RATE = 2e-4
 
 tf.random.set_seed(123456)
+
+
+class Pool:
+    def __init__(self, size=50):
+        self.size = size
+        self.images = []
+
+    def read(self, step, image):
+        """
+        read images with random sampling
+        return image unchanged if step < size=50
+        """
+
+        if step < self.size:
+            return image
+
+        if random.randint(0, 1) == 0:
+            return image
+
+        index = random.randint(0, self.size - 1)
+        return self.images[index]
+
+    def write(self, step, image):
+        """
+        update images
+        """
+
+        if len(self.images) > self.size:
+            self.images.pop(-1)
+
+        self.images.append(image)
 
 
 def readdecode(filename):
@@ -32,6 +67,13 @@ def readdecode(filename):
     return image
 
 
+def disloss(dis_y, dis_g_x):
+    loss1 = tf.reduce_mean(tf.pow(dis_y - tf.ones_like(dis_y), 2))
+    loss2 = tf.reduce_mean(tf.pow(dis_g_x, 2))
+
+    return (loss1 + loss2) * 0.5
+
+
 def identity_loss(gen_image, image):
     loss = tf.reduce_mean(tf.abs(gen_image - image))
     return LAMBDA * 0.5 * loss
@@ -39,13 +81,6 @@ def identity_loss(gen_image, image):
 
 def ganloss(d_g_x):
     return tf.reduce_mean(tf.pow(d_g_x - tf.ones_like(d_g_x), 2))
-
-
-def disloss(dis_y, dis_g_x):
-    loss1 = tf.reduce_mean(tf.pow(dis_y - tf.ones_like(dis_y), 2))
-    loss2 = tf.reduce_mean(tf.pow(dis_g_x, 2))
-
-    return (loss1 + loss2) * 0.5
 
 
 def cycleloss(f_g_x, g_f_x, x, y):
@@ -121,6 +156,85 @@ def testdataset(args):
         plt.savefig('image.png')
 
 
+class Cyclegan:
+    def __init__(self, summary):
+        self.summary = summary
+        self.ggan = model.generator()
+        self.fgan = model.generator()
+
+        self.gdis = pix2pix.discriminator(
+            norm_type='instancenorm', target=False)
+        self.fdis = pix2pix.discriminator(
+            norm_type='instancenorm', target=False)
+
+        self.opti_ggan = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+        self.opti_fgan = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+        self.opti_gdis = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+        self.opti_fdis = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+
+    @tf.function
+    def step_dis(self, x, y, xrec, yrec, step):
+        with self.summary.as_default():
+            with tf.GradientTape(persistent=True) as tape:
+                disgtruth = self.gdis(y, training=True)
+                disftruth = self.fdis(x, training=True)
+                disgfake = self.gdis(xrec)
+                disffake = self.fdis(yrec)
+
+                disgloss = disloss(disgtruth, disgfake)
+                disfloss = disloss(disftruth, disffake)
+
+            tf.summary.scalar('dgloss', disgloss, step)
+            tf.summary.scalar('dfloss', disfloss, step)
+
+        disggrad = tape.gradient(disgloss, self.gdis.trainable_variables)
+        disfgrad = tape.gradient(disfloss, self.fdis.trainable_variables)
+
+        self.opti_gdis.apply_gradients(
+            zip(disggrad, self.gdis.trainable_variables))
+        self.opti_fdis.apply_gradients(
+            zip(disfgrad, self.fdis.trainable_variables))
+
+    @tf.function
+    def step_gan(self, x, y, step):
+        with self.summary.as_default():
+            with tf.GradientTape(persistent=True) as tape:
+                g_out = self.ggan(x, training=True)
+                cycle_g = self.fgan(g_out, training=True)
+                f_out = self.fgan(y, training=True)
+                cycle_f = self.ggan(f_out, training=True)
+
+                dis_g_out = self.gdis(g_out, training=True)
+                dis_f_out = self.fdis(f_out, training=True)
+
+                cyloss = LAMBDA * cycleloss(cycle_g, cycle_f, x, y)
+                g_loss = ganloss(dis_g_out)
+                f_loss = ganloss(dis_f_out)
+
+                disgfakeloss = tf.reduce_mean(tf.pow(dis_g_out, 2))
+                disffakeloss = tf.reduce_mean(tf.pow(dis_f_out, 2))
+
+                # identity_loss_g = identity_loss(g_y, y)
+                # identity_loss_f = identity_loss(f_x, x)
+
+                total_gan_loss_g = g_loss + cyloss
+                total_gan_loss_f = f_loss + cyloss
+
+            tf.summary.scalar('gloss', g_loss, step)
+            tf.summary.scalar('floss', f_loss, step)
+            tf.summary.scalar('cyloss', cyloss, step)
+
+        g_grads = tape.gradient(g_loss, self.ggan.trainable_variables)
+        f_grads = tape.gradient(f_loss, self.fgan.trainable_variables)
+
+        self.opti_ggan.apply_gradients(
+            zip(g_grads, self.ggan.trainable_variables))
+        self.opti_fgan.apply_gradients(
+            zip(f_grads, self.fgan.trainable_variables))
+
+        return g_out, f_out
+
+
 def train(args):
     if args.gpu is None:
         print('CPU NOT SUPPORTED')
@@ -130,93 +244,25 @@ def train(args):
     train_log_dir = f'logs/{date_time}/train'
     train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 
-    @tf.function
-    def train_step_g(x, y, step):
-        with train_summary_writer.as_default():
-            with tf.GradientTape(persistent=True) as tape:
-                g_out = g_gan(x, training=True)
-                cycle_g = f_gan(g_out, training=True)
-                f_out = f_gan(y, training=True)
-                cycle_f = g_gan(f_out, training=True)
-
-                dis_g_out = dis_g(g_out, training=True)
-                dis_f_out = dis_f(f_out, training=True)
-
-                cyloss = LAMBDA * cycleloss(cycle_g, cycle_f, x, y)
-                g_loss = ganloss(dis_g_out)
-                f_loss = ganloss(dis_f_out)
-
-                g_y = g_gan(y, training=True)
-                f_x = f_gan(x, training=True)
-
-                # identity_loss_g = identity_loss(g_y, y)
-                # identity_loss_f = identity_loss(f_x, x)
-
-                total_gan_loss_g = g_loss + cyloss
-                total_gan_loss_f = f_loss + cyloss
-
-                dis_g_y = dis_g(y, training=True)
-                dis_f_y = dis_f(x, training=True)
-                dis_g_loss = disloss(dis_g_y, dis_g_out)
-                dis_f_loss = disloss(dis_f_y, dis_f_out)
-
-                dis_x_false = dis_g(x, training=True)
-                dis_y_false = dis_f(y, training=True)
-
-                dis_g_loss1 = tf.reduce_mean(tf.pow(dis_x_false, 2))
-                dis_f_loss1 = tf.reduce_mean(tf.pow(dis_y_false, 2))
-
-                dis_g_loss = (dis_g_loss1 + dis_g_loss) * 0.5
-                dis_f_loss = (dis_f_loss1 + dis_f_loss) * 0.5
-
-            g_grads = tape.gradient(g_loss, g_gan.trainable_variables)
-            f_grads = tape.gradient(f_loss, f_gan.trainable_variables)
-            dis_g_grads = tape.gradient(dis_g_loss, dis_g.trainable_variables)
-            dis_f_grads = tape.gradient(dis_f_loss, dis_f.trainable_variables)
-
-            g_gan_opti.apply_gradients(zip(g_grads, g_gan.trainable_variables))
-            f_gan_opti.apply_gradients(zip(f_grads, f_gan.trainable_variables))
-            dis_g_opti.apply_gradients(
-                zip(dis_g_grads, dis_g.trainable_variables))
-            dis_f_opti.apply_gradients(
-                zip(dis_f_grads, dis_f.trainable_variables))
-
-            tf.summary.scalar('gloss', g_loss, step)
-            tf.summary.scalar('floss', f_loss, step)
-            tf.summary.scalar('dgloss', dis_g_loss, step)
-            tf.summary.scalar('dfloss', dis_f_loss, step)
-            tf.summary.scalar('cyloss', cyloss, step)
-
-    if args.gpu:
-        print('GPU MODE')
-        g_gan = model.generator()
-        f_gan = model.generator()
-
-        dis_g = pix2pix.discriminator(
-            norm_type='instancenorm', target=False)
-        dis_f = pix2pix.discriminator(
-            norm_type='instancenorm', target=False)
-
-        g_gan_opti = tf.keras.optimizers.Adam(2e-04, BETA_1)
-        f_gan_opti = tf.keras.optimizers.Adam(2e-04, BETA_1)
-        dis_g_opti = tf.keras.optimizers.Adam(2e-04, BETA_1)
-        dis_f_opti = tf.keras.optimizers.Adam(2e-04, BETA_1)
-
     womends, mends = create_dataset_celb(args.d)
 
     batch_men = mends.take(TAKEN_NUM).shuffle(TAKEN_NUM).batch(1)
     batch_women = womends.take(TAKEN_NUM).shuffle(TAKEN_NUM).batch(1)
 
+    cyclegan = Cyclegan(train_summary_writer)
+    gpool = Pool()
+    fpool = Pool()
+
     checkpoint_path = './checkpoints/'
     ckpt = tf.train.Checkpoint(
-        g_gan=g_gan,
-        f_gan=f_gan,
-        dis_g=dis_g,
-        dis_f=dis_f,
-        g_gan_opti=g_gan_opti,
-        f_gan_opti=f_gan_opti,
-        dis_g_opti=dis_g_opti,
-        dis_f_opti=dis_f_opti
+        ggan=cyclegan.ggan,
+        fgan=cyclegan.fgan,
+        gdis=cyclegan.gdis,
+        fdis=cyclegan.fdis,
+        opti_ggan=cyclegan.opti_ggan,
+        opti_fgan=cyclegan.opti_fgan,
+        opti_gdis=cyclegan.opti_gdis,
+        opti_fdis=cyclegan.opti_fdis
     )
 
     ckpt_manager = tf.train.CheckpointManager(
@@ -231,13 +277,21 @@ def train(args):
 
         for index, (x, y) in tf.data.Dataset.zip((batch_women, batch_men)).enumerate():
             step = epoch * TAKEN_NUM + index
-            train_step_g(x, y, step)
+            g_out, f_out = cyclegan.step_gan(x, y, step)
+
+            gpool.write(step, g_out)
+            fpool.write(step, f_out)
+
+            g_out = gpool.read(step, g_out)
+            f_out = fpool.read(step, f_out)
+
+            cyclegan.step_dis(x, y, g_out, f_out, step)
 
         final_time = time.time() - init_time
 
         with train_summary_writer.as_default():
             for test in mends.skip(TAKEN_NUM + 1).take(1):
-                image = f_gan(tf.expand_dims(test, 0))
+                image = cyclegan.fgan(tf.expand_dims(test, 0))
                 stacked_image = tf.stack([image[0], test]) * 0.5 + 0.5
                 tf.summary.image('m from fm', stacked_image, epoch)
 
